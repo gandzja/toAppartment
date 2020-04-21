7010310e7e24aec613b39ed424adbdbbf9a9996f
gandzja <gandzja@gmail.com>
2020-04-21 18:53:45 +0300

    3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(function () {
   $('.slider').slick({
-    nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next arrow" /></button>',
-    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev arrow" /></button>',
+    nextArrow: '<button class="slick-arrow slick-next btn"><img src="images/next.svg" alt="next arrow" /></button>',
+    prevArrow: '<button class="slick-arrow slick-prev btn"><img src="images/prev.svg" alt="prev arrow" /></button>',
     fade: true,
+    responsive: [
+      {
+        breakpoint: 441,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
 });
